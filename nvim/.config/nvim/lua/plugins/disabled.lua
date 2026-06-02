00d7fb5c7cd84482a16eb9f0096f8041396509a9
mfellipeso <matheusprobr@gmail.com>
2026-06-02 16:38:15 -0300

    update deps

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,9 +1,9 @@
 return {
-  -- { "folke/noice.nvim", enabled = false },
-  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
-
   -- disable inlay_hints
-  { "neovim/nvim-lspconfig", opts = {
-    inlay_hints = { enabled = false },
-  } },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    }
+  },
 }
